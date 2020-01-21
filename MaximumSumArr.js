46d4b74621b62fb8d20d70ef7833e49e847608ce
Никита <khabnet47@mail.ru>
2020-01-21 11:32:02 +0300

    Скрипт который показывает как работает округления

---
+++ MaximumSumArr.js
@@ -0,0 +1,20 @@
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+function maxSequence(arr){
+    console.log(arr);
+    let arr2 = [];
+     if(arr.length == 0 || Math.max(...arr) < 0) {
+       return(0)
+     } else {
+       for(let i = 0; i <= arr.length; i++){
+        let arr1 = [];
+        for(let k = 1; k <= arr.length-i; k++){
+            arr1.push( (arr.slice( i,i+k )).reduce( (a,b) => a+b ) );
+        }
+        arr2.push(Math.max(...arr1));
+      }
+    return (Math.max(...arr2));
+     }
+}
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+// should be 6: [4, -1, 2, 1]
